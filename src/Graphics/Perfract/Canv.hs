fc4c20f9fcc602fe54ff39acf8f2dac25859ae17
Daniel Corson <danl@alum.mit.edu>
2016-03-19 14:42:45 -0400

    cp

--- src/Graphics/Perfract/Canv.hs
+++ src/Graphics/Perfract/Canv.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 -- Rational canvas with showing to screen and saving to file.
 
 module Graphics.Perfract.Canv where
@@ -5,29 +7,42 @@ module Graphics.Perfract.Canv where
 import Codec.Picture
 import Control.Applicative
 import Control.Monad
+import Data.Ratio
+import Data.Word
 
 import Graphics.Perfract.Tupelo
 import qualified Data.Vector.Mutable as MVec
 import qualified Data.Vector.Storable as SVec
 import qualified Data.Vector.Storable.Mutable as MSVec
 
-data Canv = Canv
+-- This represents a pixel channel (eg. R, G, or B) during manipulation,
+-- where higher working precision (than the final 0-255) allows more accuracy.
+class DeepSubpixel a where
+    toSubpixel :: a -> Word8
+
+instance DeepSubpixel Float where
+    toSubpixel = round . (255 *)
+
+instance DeepSubpixel (Ratio Integer) where
+    toSubpixel = round . (255 *)
+
+data Canv a = Canv
     { cW :: !Int
     , cH :: !Int
-    , cV :: MVec.IOVector (ABC Rational)
+    , cV :: MVec.IOVector (ABC a)
     }
 
-newCanv :: Int -> Int -> IO Canv
+newCanv :: DeepSubpixel a => Int -> Int -> IO (Canv a)
 newCanv w h = do
     v <- MVec.new (w * h)
     MVec.set v (ABC 0 0 0)
     return $ Canv w h v
 
-saveCanv :: FilePath -> Canv -> IO ()
+saveCanv :: DeepSubpixel a => FilePath -> Canv a -> IO ()
 saveCanv pngF (Canv w h v) = do
     v2m <- MSVec.new (w * h * 3)
     forM_ [0 .. w * h - 1] $ \i -> do
-        ABC r g b <- abcMap (round . (255 *)) <$> MVec.read v i
+        ABC r g b <- abcMap toSubpixel <$> MVec.read v i
         let i3 = i * 3
             i31 = i3 + 1
             i32 = i3 + 2
--- src/Main.hs
+++ src/Main.hs
@@ -5,22 +5,8 @@ import System.Environment
 
 import Graphics.Perfract
 
---  . . . .
--- . o   o  .
--- .o/\  /\o.
---   \/__\/
---    |  |
---    |__|
 skyTri :: RecFig
 skyTri = RecFig
-    {-
-    []
-    [XY 0 0, XY (-100) 200, XY 100 300, XY 300 200, XY 200 0]
-    [ Prz (XY (-100) 200) (ratRot $ -0.11) (0.25)
-    -- , Prz (XY 100 300) (ratRot $ -0.07)  (0.35)
-    -- , Prz (XY 300 200) (ratRot $ 0.17)  (0.45)
-    ]
-    -}
     (Vec.fromList
         [ XY 0 0
         , XY (-300) 200
